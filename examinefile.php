3f6f8138e4ebc33d24a98dba1ea8425920b9dd0b
bernorieder <berno.rieder@gmail.com>
2017-01-09 11:47:19 +0100

    reddit update

--- examinefile.php
+++ examinefile.php
@@ -16,6 +16,8 @@ if(preg_match("/authorChannelId/",$buffer)) { $type = "youtube"; }
 if(preg_match("/comment_like_count/",$buffer)) { $type = "facebook_topcomments"; }
 if(preg_match("/position/",$buffer)) { $type = "facebook"; }
 if(preg_match("/in_reply_to_status_id/",$buffer)) { $type = "twitter"; }
+if(preg_match("/post_ups/",$buffer)) { $type = "reddit_comments"; }
+
 
 $test = array("type" => $type);
 
